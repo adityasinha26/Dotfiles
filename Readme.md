15affa4878da656e8c83acc22a31a98784fe3aff
adityasinha26 <aditya.sinha@zomato.com>
2020-09-30 10:08:33 +0530

    Minimal set up on PopOS
    1. Added Kmonad configuration
    
    Currently it maps caps lock key to function as escape when tapped and as
    left control when held.
    2. Enabled mouse movement in tmux
    3. Added basic neovim config

---
+++ Readme.md
@@ -0,0 +1,9 @@
+Bare minimum set up on a new computer.
+
+1. SymLink the dotfiles 
+```
+ln -s ~/Dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/Dotfiles/.init.lua ~/.config/nvim/init.lua
+```
+2. Install neovim
---
+++ init.lua
@@ -0,0 +1,39 @@
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Basic options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 300
+
+-- Clipboard (system)
+vim.opt.clipboard = "unnamedplus"
+
+-- Keymaps
+local keymap = vim.keymap.set
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h")
+keymap("n", "<C-j>", "<C-w>j")
+keymap("n", "<C-k>", "<C-w>k")
+keymap("n", "<C-l>", "<C-w>l")
+
+-- Basic autocommand example
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
